c7b44b1649fba7af8a81d550cb1c6f1ca93afe84
GameExplorer <GameExplorer@users.noreply.github.com>
2023-10-25 17:02:14 +0200

    Section-2.23
    Understanding key attribute

---
+++ Section-2.23/app.js
@@ -0,0 +1,26 @@
+let vm = Vue.createApp({
+  data() {
+    return {
+      people: [
+        {
+          name: 'John',
+          message: 'Hello world!'
+        },
+        {
+          name: 'Rick',
+          message: 'I like pie.'
+        },
+        {
+          name: 'Amy',
+          message: 'Skydiving is fun!'
+        }
+      ]
+    }
+  },
+  methods: {
+    move() {
+      const first = this.people.shift()
+      this.people.push(first)
+    }
+  }
+}).mount('#app')
