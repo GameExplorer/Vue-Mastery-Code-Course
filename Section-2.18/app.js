c986a90e17ab87e78114302efff0dcdf34e8095a
GameExplorer <GameExplorer@users.noreply.github.com>
2023-10-25 16:04:43 +0200

    Section-2.18
    - Binding Styles

---
+++ Section-2.18/app.js
@@ -0,0 +1,16 @@
+let vm = Vue.createApp({
+    data() {
+        return {
+            isPurple: false,
+            selectedColor: '',
+            size: 150,
+        }
+    },
+    computed: {
+        circle_classes() {
+            return {
+                purple: this.isPurple,
+            }
+        }
+    }
+}).mount('#app')
