178be3e21dd693dc985d97e7dec830bef7469e6f
GameExplorer <GameExplorer@users.noreply.github.com>
2023-11-02 21:18:15 +0100

    Slots
    Added Slots

---
+++ Section-6.8/slots/src/App.vue
@@ -0,0 +1,46 @@
+<script>
+import Forms from "./components/Forms.vue";
+export default {
+  name: "App",
+  components: {
+    Forms,
+  },
+
+  data() {
+    return {
+      help: "This is some help text",
+    }
+  }
+}
+</script>
+
+<template>
+  <Forms>
+    <template v-slot:help>
+      <p>{{  help }}</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="email" />
+      <input type="text" placeholder="username" />
+      <input type="password" placeholder="password" />
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+    <p>Random text</p>
+  </Forms>
+  <Forms>
+    <template  v-slot:help>
+      <p>Contact help</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="name" />
+      <input type="text" placeholder="message" />
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+  </Forms>
+</template>
+
+
---
+++ Section-6.8/slots/src/components/Forms.vue
@@ -0,0 +1,22 @@
+<template>
+    <form>
+        <div class="help">
+            <slot name="help"></slot>
+        </div>
+        <div class="fields">
+            <slot name="fields"></slot>
+        </div>
+        <div class="buttons">
+            <slot name="buttons"></slot>
+        </div>
+        <slot></slot>
+    </form>
+</template>
+
+<script>
+
+</script>
+
+<style >
+
+</style>
