b42fa16266e2308c6722c48b08637755062c7bb0
GameExplorer <GameExplorer@users.noreply.github.com>
2023-11-03 17:38:55 +0100

    CSS Animations

--- Section-7.1/AnimationsTransitions/src/App.vue
+++ Section-7.1/AnimationsTransitions/src/App.vue
@@ -3,7 +3,7 @@ export default {
   name: 'App',
   data() {
     return {
-      flag: false,
+      flag: true,
     }
   }
 }
@@ -12,13 +12,25 @@ export default {
 <template>
   <button type="button" @click="flag = !flag">Toggle</button>
 
+  <!-- Transitions 
   <Transition name="fade" mode="out-in">
     <h2 v-if="flag" key="main">Hello Vue!</h2>
     <h2 v-else key="secondary"> Another Vue!</h2>
-</Transition>
+  </Transition>
+  -->
+
+  <Transition name="zoom" type="animation" appear>
+    <h2 v-if="flag">Hello</h2>
+  </Transition>
 </template>
 
-<style >
+<style>
+h2 {
+  width: 400px;
+  padding: 2em;
+  margin: 2em;
+}
+
 .fade-enter-from {
   opacity: 0;
 }
@@ -30,5 +42,41 @@ export default {
 .fade-leave-to {
   transition: all 0.25s linear;
   opacity: 0;
+}
+
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+  transition: all 1s linear;
+}
+
+.zoom-leave-active {
+  animation: zoom-out 1s linear forwards;
+  transition: all 1s linear;
+}
+
+.zoom-enter-from {
+  opacity: 0;
+}
+
+.zoom-leave-to {
+  opacity: 0;
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0,0);
+  }
+  to {
+    transform: scale(1,1);
+  }
+}
+
+@keyframes zoom-out {
+  from {
+    transform: scale(1,1);
+  }
+  to {
+    transform: scale(0,0);
+  }
 }
 </style>
