276ef7aca18165ab22d7f006b4792debb124ddd9
GameExplorer <GameExplorer@users.noreply.github.com>
2023-11-03 17:07:01 +0100

    Dynamic Components

---
+++ Section-6.11/dynamic component/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+
+export default {
+  name: "App",
+  components: {
+    Home,
+    About
+  },
+  data() {
+    return {
+      componentName: "Home",
+    }
+  }
+}
+</script>
+
+<template>
+  <select v-model="componentName">
+    <option value="Home">Home</option>
+    <option value="About">About</option>
+  </select>
+
+  <keep-alive>
+    <component :is="componentName"></component>
+  </keep-alive>
+  
+</template>
+
+<style scoped>
+
+</style>
