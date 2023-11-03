f5b6fdcdf545cd7a3ab0d4f5a52cc08bc5b9d93b
GameExplorer <GameExplorer@users.noreply.github.com>
2023-11-03 17:21:52 +0100

    Animations-1

---
+++ Section-7.1/AnimationsTransitions/src/App.vue
@@ -0,0 +1,33 @@
+<script >
+export default {
+  name: 'App',
+  data() {
+    return {
+      flag: false,
+    }
+  }
+}
+</script>
+
+<template>
+  <button type="button" @click="flag = !flag">Toggle</button>
+
+  <Transition name="fade">
+    <h2 v-if="flag">Hello Vue!</h2>
+</Transition>
+</template>
+
+<style >
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.25s linear;
+}
+
+.fade-leave-to {
+  transition: all 0.25s linear;
+  opacity: 0;
+}
+</style>
