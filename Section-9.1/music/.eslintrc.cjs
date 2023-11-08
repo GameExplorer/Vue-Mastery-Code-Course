d7ac6e64d9f62fb5bdb87174e1c5501c856f40b0
GameExplorer <GameExplorer@users.noreply.github.com>
2023-11-08 17:35:56 +0100

    85 - 97

---
+++ Section-9.1/music/.eslintrc.cjs
@@ -0,0 +1,31 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  overrides: [
+    {
+      files: [
+        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
+      ],
+      'extends': [
+        'plugin:cypress/recommended'
+      ]
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  env: {
+    node: true,
+  },
+
+  rules: {
+    "vue/multi-word-component-names": "off" // Multiword disabling
+  }
+}
