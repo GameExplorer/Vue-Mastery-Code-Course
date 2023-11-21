71306ceb214dd3ad691038943231155b0159f553
GameExplorer <GameExplorer@users.noreply.github.com>
2023-11-21 08:54:55 +0100

    Slots
    A little bit of slots

---
+++ Section-10.12/Slots/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <hello-world #default="{ user, favorites }">
+      <p>Hello {{ user.name }}. I like {{ favorites[0] }}.</p>
+    </hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ Section-10.12/Slots/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <slot :user="user" :favorites="favorites"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      user: {
+        name: "John",
+        age: 25,
+      },
+      favorites: ["Pizza", "Marbles", "Birds"],
+    };
+  },
+}
+</script>
