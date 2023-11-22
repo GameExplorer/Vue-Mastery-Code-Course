07fdf031161fda7d0e75a901a903885bce8639ab
GameExplorer <GameExplorer@users.noreply.github.com>
2023-11-22 17:44:25 +0100

    Up to 147

--- Section-9.1/music/src/components/Header.vue
+++ Section-9.1/music/src/components/Header.vue
@@ -3,11 +3,18 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/" exact-active-class="no-active">Music</router-link>
+      
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
+          <li>
+            <router-link class="px-2 text-white" to="/about">About</router-link>
+          </li>
+          <li>
+            <router-link class="px-2 text-white" to="/manage">Manage</router-link>
+          </li>
           <!-- Navigation Links -->
           <li v-if="!userStore.userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
--- Section-9.1/music/src/router/index.js
+++ Section-9.1/music/src/router/index.js
@@ -1,23 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from "@/views/Home.vue"
+import About from "@/views/About.vue"
+import Manage from "@/views/Manage.vue"
+
+const routes = [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/about',
+    compoment: About,
+  },
+  {
+    path: '/manage',
+    component: Manage,
+  }
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/About.vue')
-    }
-  ]
+  history: createWebHistory(import.meta.env.BASE_URL), //history mode
+  routes,
+  linkExactActiveClass: "text-yellow-500",
 })
 
 export default router
