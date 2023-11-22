9d8a102f465c51611f7362a607eaaafa1afaa5b9
GameExplorer <GameExplorer@users.noreply.github.com>
2023-11-22 18:04:40 +0100

    Up to 151
    routes

--- Section-9.1/music/src/router/index.js
+++ Section-9.1/music/src/router/index.js
@@ -5,16 +5,28 @@ import Manage from "@/views/Manage.vue"
 
 const routes = [
   {
+    name: 'home',
     path: '/',
     component: Home
   },
   {
+    name: "about",
     path: '/about',
     compoment: About,
   },
   {
-    path: '/manage',
+    name: "manage",
+    //alias: '/manage', // router will render manage component when user visits /manage-music
+    path: '/manage-music',
     component: Manage,
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' },
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' },
   }
 ];
 
@@ -23,5 +35,10 @@ const router = createRouter({
   routes,
   linkExactActiveClass: "text-yellow-500",
 })
+
+router.beforeEach((to, from, next) => { //to: where we are going, from: where we are coming from, next: function to call to continue
+  console.log(to, from);
+  next();
+}); 
 
 export default router
