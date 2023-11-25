e8cdc901f9cb105e5dc79f255d56f57e5571dd59
GameExplorer <GameExplorer@users.noreply.github.com>
2023-11-25 18:53:35 +0100

    Finished 14th Section - COMPLETED

--- Section-9.1/music/src/components/SongItem.vue
+++ Section-9.1/music/src/components/SongItem.vue
@@ -8,10 +8,14 @@
     </div>
 
     <div class="text-gray-600 text-lg">
-      <span class="comments">
+      <router-link custom 
+      :to="{ name: 'song', params: { id: song.docID }, hash: '#comments' }"
+      v-slot="{ navigate }">
+      <span class="comments" @click="navigate">
         <i class="fa fa-comments text-gray-600"></i>
         {{  song.comment_count }}
       </span>
+      </router-link>
     </div>
   </li>
 </template>
