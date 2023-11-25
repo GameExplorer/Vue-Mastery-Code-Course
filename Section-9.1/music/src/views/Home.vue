5588c052604367e75bcd168841de5e29a63ded5d
GameExplorer <GameExplorer@users.noreply.github.com>
2023-11-25 20:07:23 +0100

    Directives - Completed

--- Section-9.1/music/src/views/Home.vue
+++ Section-9.1/music/src/views/Home.vue
@@ -26,10 +26,12 @@
     <!-- Main Content -->
     <section class="container mx-auto">
       <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-        <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+        <div
+          class="px-6 pt-6 pb-5 font-bold border-b border-gray-200"
+          v-icon-secondary="{ icon: 'headphones-alt', right: true }"
+        >
           <span class="card-title">Songs</span>
           <!-- Icon -->
-          <i class="fa fa-headphones-alt float-right text-green-400 text-xl"></i>
         </div>
         <!-- Playlist -->
         <ol id="playlist">
@@ -44,11 +46,15 @@
 <script>
 import AppSongItem from '@/components/SongItem.vue'
 import { songsCollection } from '@/includes/firebase'
+import IconSecondary from '@/directives/icon-secondary'
 
 export default {
   name: 'Home',
   components: {
     AppSongItem
+  },
+  directives: {
+    'icon-secondary': IconSecondary,
   },
   data() {
     return {
