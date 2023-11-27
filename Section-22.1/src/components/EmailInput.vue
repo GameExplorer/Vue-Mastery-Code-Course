dd99069c7242aaa05cdda99988ad13c8e0f57c3e
GameExplorer <GameExplorer@users.noreply.github.com>
2023-11-27 20:41:17 +0100

    up to 269

---
+++ Section-22.1/src/components/EmailInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <input
+    type="email"
+    class="form-control"
+    placeholder="E-mail"
+    :value="email"
+    @input="update($event)"
+    :class="{
+      'is-valid': validateEamil(email),
+      'is-invalid': !validateEamil(email),
+    }"
+  />
+</template>
+
+<script>
+import validateEamil from "../validate-email";
+export default {
+  name: "EmailInput",
+  props: ["email"],
+  methods: {
+    update() {
+      this.$emit("update:email", $event.target.value);
+      },
+    validateEamil,
+  },
+};
+</script>
