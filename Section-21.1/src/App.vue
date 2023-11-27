f94fbc19f46712e29335ca422c8c1242d5e4a10d
GameExplorer <GameExplorer@users.noreply.github.com>
2023-11-27 17:11:24 +0100

    Mixin

---
+++ Section-21.1/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div class="scrollPosition">{{  offset }}</div>
+  </div>
+</template>
+
+<script>
+import mixin from '@/mixin';
+
+export default {
+  name: "App",
+  mixins: [mixin],
+  data() {
+    return {
+      offset: 0,
+    }
+  },
+  mounted() {
+    console.log("App mounted lifecycle function")
+  }
+};
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 3000px;
+  position: relative;
+}
+
+.scrollPosition {
+  position: fix;
+  left: 10px;
+  right: 10px;
+}
+</style>
