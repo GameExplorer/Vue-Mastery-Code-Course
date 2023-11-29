e9115a87786a1caa2ee06ba822198e428411d39b
GameExplorer <GameExplorer@users.noreply.github.com>
2023-11-29 21:15:55 +0100

    misc

--- Section-8.1/quiz/src/App.vue
+++ Section-8.1/quiz/src/App.vue
@@ -1,24 +1,35 @@
-
-
 <template>
-<div class="ctr">
-  <Transition name="fade" mode="out-in">
-    <Questions v-if="questionsAnswered < questions.length" 
-    :questions="questions" :questionsAnswered="questionsAnswered" @question-answered="questionAnswered"/> <!-- v-if checks if we answered questions, we pass down the question array -->
-    
-    <Results v-else :results="results" :totalCorrect="totalCorrect" /> <!-- if we answered the question show the results -->
-  </Transition>
-  <button type="button" class="reset-btn" @click.prevent="reset"
-  v-if="questionsAnswered === questions.length">Reset</button> <!-- reset button -->
-</div>
+  <div class="ctr">
+    <Transition name="fade" mode="out-in">
+      <Questions
+        v-if="questionsAnswered < questions.length"
+        :questions="questions"
+        :questionsAnswered="questionsAnswered"
+        @question-answered="questionAnswered"
+      />
+      <!-- v-if checks if we answered questions, we pass down the question array -->
+
+      <Results v-else :results="results" :totalCorrect="totalCorrect" />
+      <!-- if we answered the question show the results -->
+    </Transition>
+    <button
+      type="button"
+      class="reset-btn"
+      @click.prevent="reset"
+      v-if="questionsAnswered === questions.length"
+    >
+      Reset
+    </button>
+    <!-- reset button -->
+  </div>
 </template>
 
 <script>
-import Questions from './components/Questions.vue'
-import Results from './components/Results.vue'
+import Questions from "./components/Questions.vue";
+import Results from "./components/Results.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Questions,
     Results,
@@ -28,81 +39,81 @@ export default {
       questionsAnswered: 0, // we haven't answered any questions yet
       totalCorrect: 0, // we haven't answered any questions correctly yet
       questions: [
-          {
-              q: 'What is 2 + 2?', 
-              answers: [
-                  {
-                      text: '4',
-                      is_correct: true
-                  },
-                  {
-                      text: '3',
-                      is_correct: false 
-                  },
-                  {
-                      text: 'Fish',
-                      is_correct: false 
-                  },
-                  {
-                      text: '5',
-                      is_correct: false 
-                  }
-              ] 
-          },
-          { 
-              q: 'How many letters are in the word "Banana"?', 
-              answers: [
-                  {
-                      text: '5',
-                      is_correct: false
-                  },
-                  {
-                      text: '7',
-                      is_correct: false 
-                  },
-                  {
-                      text: '6',
-                      is_correct: true 
-                  },
-                  {
-                      text: '12',
-                      is_correct: false 
-                  }
-              ] 
-          },
-          { 
-              q: 'Find the missing letter: C_ke', 
-              answers: [
-                  {
-                      text: 'e',
-                      is_correct: false
-                  },
-                  {
-                      text: 'a',
-                      is_correct: true 
-                  },
-                  {
-                      text: 'i',
-                      is_correct: false 
-                  }
-              ] 
-          },
+        {
+          q: "What is 2 + 2?",
+          answers: [
+            {
+              text: "4",
+              is_correct: true,
+            },
+            {
+              text: "3",
+              is_correct: false,
+            },
+            {
+              text: "Fish",
+              is_correct: false,
+            },
+            {
+              text: "5",
+              is_correct: false,
+            },
+          ],
+        },
+        {
+          q: 'How many letters are in the word "Banana"?',
+          answers: [
+            {
+              text: "5",
+              is_correct: false,
+            },
+            {
+              text: "7",
+              is_correct: false,
+            },
+            {
+              text: "6",
+              is_correct: true,
+            },
+            {
+              text: "12",
+              is_correct: false,
+            },
+          ],
+        },
+        {
+          q: "Find the missing letter: C_ke",
+          answers: [
+            {
+              text: "e",
+              is_correct: false,
+            },
+            {
+              text: "a",
+              is_correct: true,
+            },
+            {
+              text: "i",
+              is_correct: false,
+            },
+          ],
+        },
       ],
       results: [
-          {
-              min: 0,
-              max: 2,
-              title: "Try again!",
-              desc: "Do a little more studying and you may succeed!"
-          },
-          {
-              min: 3,
-              max: 3,
-              title: "Wow, you're a genius!",
-              desc: "Studying has definitely paid off for you!"
-          }
-      ]
-    }
+        {
+          min: 0,
+          max: 2,
+          title: "Try again!",
+          desc: "Do a little more studying and you may succeed!",
+        },
+        {
+          min: 3,
+          max: 3,
+          title: "Wow, you're a genius!",
+          desc: "Studying has definitely paid off for you!",
+        },
+      ],
+    };
   },
   methods: {
     questionAnswered(is_correct) {
@@ -115,10 +126,7 @@ export default {
     reset() {
       this.questionsAnswered = 0; // reset the questionsAnswered
       this.totalCorrect = 0; // reset the totalCorrect
-    }
-  }
-}
+    },
+  },
+};
 </script>
-<style>
-
-</style>
--- Section-9.1/music/dev-dist/sw.js
+++ Section-9.1/music/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jqkoson452o"
+    "revision": "0.n19rvdpip3o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
